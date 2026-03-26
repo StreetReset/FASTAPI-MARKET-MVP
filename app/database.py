2f1c7a11af4b093c9864c53f2fba216af2d82ced
StreetReset <youngapulya@gmail.com>
2026-03-26 12:06:05 +0300

    Initial commit without sec

--- app/database.py
+++ app/database.py
@@ -1,22 +1,10 @@
-#----------------SYNC----------------
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, DeclarativeBase
-# DATABASE_URL = "sqlite:///ecommerce.db"
-
-# engine = create_engine(DATABASE_URL, echo=True)
-
-# SessionLocal = sessionmaker(
-#     autocommit = False,
-#     autoflush= False,
-#     bind = engine
-# )
-
-#----------------ASYNC----------------
-
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "postgresql+asyncpg://ecommerce_user:sewjr3@localhost:5432/ecommerce_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 async_engine = create_async_engine(DATABASE_URL, echo = True) #False для проды
 async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
